c45fbc7218cc5b522d58b03df2812d27fbdc2a56
PyPack <serg.karetnuk333@gmail.com>
2024-05-30 20:11:50 +0200

    1.5 add table bookings

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -18,6 +18,8 @@ from app.database import Base
 from app.hotels.models import Hotel
 from app.users.models import User
 from app.rooms.models import Room
+from app.bookings.models import Booking
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
