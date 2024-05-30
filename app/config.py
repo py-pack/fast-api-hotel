e5f79cc4f1daa39a23b9de4da70934cc32ad0296
PyPack <serg.karetnuk333@gmail.com>
2024-05-30 17:20:02 +0200

    1.5 add table users

--- app/config.py
+++ app/config.py
@@ -22,5 +22,3 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
-
-print(settings.DATABASE_URL)
--- app/migrations/env.py
+++ app/migrations/env.py
@@ -16,6 +16,7 @@ sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
 from app.config import settings
 from app.database import Base
 from app.hotels.models import Hotel
+from app.users.models import User
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
