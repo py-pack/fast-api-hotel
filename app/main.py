f5ef4e4d7fe43649869ef7ca044b4dede6bf75ab
PyPack <serg.karetnuk333@gmail.com>
2024-05-30 12:42:24 +0200

    completed - 1.4 Валидация данных с Pydantic

--- app/main.py
+++ app/main.py
@@ -1,8 +1,54 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Depends
+from typing import Optional, List, Dict, Union
+from datetime import date
+
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class SHotel(BaseModel):
+    adress: str
+    name: str
+    start: int
+
+
+class SHotelSearchArgs:
+    def __init__(
+        self,
+        location: str,
+        date_from: date,
+        date_to: date,
+        has_spa: Optional[bool] = None,
+        start: Optional[int] = Query(None, ge=1, le=5),
+    ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.has_spa = has_spa
+        self.start = start
+
+
 @app.get("/hotels")
-def get_hotels():
-    return "Отель Бридж резорт 5 звезд"
+def get_hotels(
+    search_args: SHotelSearchArgs = Depends()
+) -> list[dict[str, Union[str, int]]]:
+    hotels = [
+        {
+            "adress": "st. Gagarina, 1, Altay",
+            "name": "Super Hotel",
+            "start": 4
+        },
+    ]
+    return hotels
+
+
+class SBooking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+@app.post("/bookings")
+def get_bookings(booking: SBooking):
+    pass
